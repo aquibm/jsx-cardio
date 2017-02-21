4c4db7503be0ad46c5c31b46a9f95b6c5dd7d226
Aquib Master <aquibmaster@gmail.com>
2017-02-22 07:37:20 +1300

    Add and configure options addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,10 @@
 import { configure } from '@kadira/storybook';
+import { setOptions } from '@kadira/storybook-addon-options';
+
+setOptions({
+    name: 'JSX Cardio',
+    url: 'https://github.com/aquibm/jsx-cardio'
+});
 
 function loadStories() {
   require('../src/stories');
