2812b115d9266bdd2b339684aa6b5ca8e487a752
Aquib Master <aquibmaster@gmail.com>
2017-02-24 22:36:49 +1300

    Remove default button story

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import { storiesOf, action } from '@kadira/storybook'
 
-import Button from './Button'
 import Welcome from './Welcome'
 import AsyncButton from './async-button'
 
 storiesOf('Welcome', module).add('', () => (
     <Welcome />
-))
-
-storiesOf('Button', module).add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-)).add('with some emoji', () => (
-    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
 ))
 
 storiesOf('Async Button', module).add('', () => (
