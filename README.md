b3cf4ff363d1ec63b06f2853cb9d0ad71b352d85
Aquib Master <aquibmaster@gmail.com>
2017-02-24 22:49:39 +1300

    Update readme and about

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ## JSX Cardio
-> Practicing me some jsx and experimenting with different ways to style components
+> JSX practice + mad science experiments
--- src/stories/about/index.jsx
+++ src/stories/about/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = ({}) => (
     <div style={{fontFamily: 'Helvetica, Arial, sans-serif'}}>
         <h1>JSX Cardio</h1>
-        <p>Built so I can practice me some jsx &amp; experiment with different ways to style components.</p>
+        <p>Built so I can practice jsx &amp; experiment with different ways to style components.</p>
     </div>
 )
 
