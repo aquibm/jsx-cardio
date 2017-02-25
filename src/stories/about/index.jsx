2becd0ecc721f4e72ddac3deb3f70dc541e1c95e
Aquib Master <aquibmaster@gmail.com>
2017-02-25 22:11:37 +1300

    Add and configured styled-jsx

--- src/stories/about/index.jsx
+++ src/stories/about/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const About = ({}) => (
-    <div style={{fontFamily: 'Helvetica, Arial, sans-serif'}}>
+    <div className="root">
         <h1>JSX Cardio</h1>
         <p>Built so I can practice jsx &amp; experiment with different ways to style components.</p>
+
+        <style jsx>{`
+            .root {
+                font-family: 'Helvetica Neue', Roboto, 'Segoe UI', Arial, sans-serif;
+            }
+        `}</style>
     </div>
 )
 
