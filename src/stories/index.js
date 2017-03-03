41cc23039174ccbcd80acb80b42fdb156c5d4933
Aquib Master <aquibmaster@gmail.com>
2017-03-03 21:55:35 +1300

    Add storybook host

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { storiesOf, action } from '@kadira/storybook'
+import { host } from 'storybook-host'
 
 import About from './about'
 import AsyncButton from './async-button'
@@ -8,6 +9,11 @@ storiesOf('About', module).add('', () => (
     <About />
 ))
 
-storiesOf('Async Button', module).add('', () => (
-    <AsyncButton onClick={action('clicked')} onSave={action('saved')} />
-))
+storiesOf('Async Button', module)
+    .addDecorator(host({
+        title: 'Button that shows the state of an asynchronous request',
+        align: 'center'
+    }))
+    .add('', () => (
+        <AsyncButton onClick={action('clicked')} onSave={action('saved')} />
+    ))
