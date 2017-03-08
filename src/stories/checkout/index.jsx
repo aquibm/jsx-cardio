b0bcf9a7d20265bbf61de15501ad98c8187c7025
Aquib Master <aquibmaster@gmail.com>
2017-03-09 07:44:44 +1300

    Add base structure for the info pane

--- src/stories/checkout/index.jsx
+++ src/stories/checkout/index.jsx
@@ -4,7 +4,17 @@ import styles from './styles'
 const Checkout = ({}) => (
     <div className="root">
         <div className="info">
-            Noot
+            <h1>Upgrade to Awesome!</h1>
+
+            <p>Unlock more room in your Buffer, extra social accounts and more!</p>
+
+            <h2>$250</h2>
+            <p><strong>15% discount</strong> compared to the monthly plan</p>
+
+            <div className="toggle-button">
+                <span>Monthly</span>
+                <span>Yearly</span>
+            </div>
         </div>
 
         <div className="payment">
